1e2400418696b990777f0ca4960db4315e9f7b6b
Ariane Camilo <arianecamilo.alves@gmail.com>
2018-09-15 16:55:07 -0300

    adicionando bootstrap Vue

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,18 +1,31 @@
 <template>
     <div>
         <h1>Posts</h1>
-    <div v-for="post in postList" :key="post._id">
-        <p>
-            <span v-text="post.user"> </span>
-            <span v-text="post.content"></span>
-        </p>
-    </div>
+        <div v-for="post in postList" :key="post._id">
+            <!-- <p>
+                <span v-text="post.user"> </span>
+                <span v-text="post.content"></span>
+            </p> -->
+            <b-card>
+                <b-media>
+                    <b-img slot="aside" blank blank-color="#ccc" width=64 alt="placeholder" />
+                    <h5 class="mt-0" v-text="post.user"></h5>
+                    <p v-text="post.content"></p>
+                </b-media>
+                <div class="btn-card">
+                    <b-button size="sm" class="my-2 my-sm-0" type="submit">Remover</b-button>
+                </div>
+            </b-card>
+        </div>
     </div>
 </template>
 
 <script>
 /* eslint-disable */
 import { mapGetters, mapActions } from 'vuex'
+import HelloWorld from './HelloWorld'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 export default {
   name: 'posts',
--- src/main.js
+++ src/main.js
@@ -3,11 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import BootstrapVue from 'bootstrap-vue'
 import store from './store/store'
 
 Vue.config.productionTip = false
-
+Vue.use(BootstrapVue)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
